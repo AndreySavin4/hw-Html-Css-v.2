2892f1ed426997651199d08c35151f9469485897
Andrey <Deymonlalkevich@gmail.com>
2024-01-08 21:47:09 +0200

    update photo and adaptive mobile first page

--- js/modal.js
+++ js/modal.js
@@ -3,14 +3,12 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
-    modalMobile: document.querySelector("[data-mobile-modal]"),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
   function toggleModal() {
-    refs.modalMobile.classList.toggle("is-hidden");
     refs.modal.classList.toggle("is-hidden");
   }
+
+  refs.openModalBtn.addEventListener("click", toggleModal());
+  refs.closeModalBtn.addEventListener("click", toggleModal());
 })();
